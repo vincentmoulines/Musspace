ba43610309b89656e01334e570f12669750e0da7
Michael Lande <miclande@gmail.com>
2017-08-01 09:18:24 +0200

    Signup now possible via passport

--- models/user.js
+++ models/user.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  genre      :String,
-  age        : Number
-  email      : String,
-  username   : String,
-  password   : String,
+  genre: String,
+  age: Number,
+  email: String,
+  username: String,
+  password: String,
   description: String,
-  imgUrl     : { type: String, default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250" }
+  imgUrl: {
+    type: String,
+    default:
+      "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250"
+  }
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
