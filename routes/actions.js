4d3efc99c2ff9291cea82ca931e6dfe63278cdf2
mlande <miclande@gmail.com>
2017-08-02 19:18:05 +0200

    more stuff

--- routes/actions.js
+++ routes/actions.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const song = require("../models/song");
+const Song = require("../models/song");
 //const TYPES = require("../models/song-types");
 const router = express.Router();
 const passport = require("passport");
@@ -8,7 +8,22 @@ const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
 const TYPES = {};
 
 router.get("/new", ensureLoggedIn(), (req, res) => {
+  if (req.user.role !== "artist") {
+    res.redirect("/dashboard");
+  }
   res.render("actions/new-music", { types: TYPES });
+});
+
+router.post("/new", (req, res) => {
+  const { link, title } = req.body;
+  const authorID = req.user._id;
+  const newSong = new Song({ url: link, title, author: authorID });
+  newSong.save((err, song) => {
+    if (err) {
+      return err;
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
